4a26b4db118142c09d8daf4cc600158a2c23a1a3
Bhavik <bhavik@Bhavik-Kiri>
2017-01-10 11:35:24 +0530

    Fix #23
    The error message has been chnaged if any duplicated record found for the exporting
    
    Note: Please properly check the test connection, and all settings related tests.

--- salesforce-crm-for-abandoned-cart/assets/js/wcap_salesforce.js
+++ salesforce-crm-for-abandoned-cart/assets/js/wcap_salesforce.js
@@ -1,108 +1,106 @@
 jQuery(function( $ ) {
 
-	var wcap_agile_connection_established = '';
+	var wcap_sf_connection_established = '';
 
-	$ ( '#wcap_agile_test' ).on( 'click', function( e ) {
+	$ ( '#wcap_salesforce_test' ).on( 'click', function( e ) {
 		e.preventDefault();
-		var wcap_agile_user_name = $("#wcap_agile_user_name").val();
-		var wcap_agile_domain    = $("#wcap_agile_domain").val();
 
-		var wcap_agile_token     = $("#wcap_agile_security_token").val();
+		var wcap_sf_user_name = $("#wcap_salesforce_user_name").val();
+		var wcap_sf_password  = $("#wcap_salesforce_password").val();
+		var wcap_sf_token     = $("#wcap_salesforce_security_token").val();
+        
         var data = {
-            wcap_agile_user_name: wcap_agile_user_name,
-            wcap_agile_domain: wcap_agile_domain,
-            wcap_agile_token: wcap_agile_token,
-            action: 'wcap_agile_check_connection'
+            wcap_sf_user_name: wcap_sf_user_name,
+            wcap_sf_password: wcap_sf_password,
+            wcap_sf_token: wcap_sf_token,
+            action: 'wcap_salesforce_check_connection'
         };
-        $( '#wcap_agile_test_connection_ajax_loader' ).show();
-        $.post( wcap_agile_params.ajax_url, data, function( response ) {
-        	var wcap_check_string = response.indexOf("successfuly established");
+        
+        $( '#wcap_salesforce_test_connection_ajax_loader' ).show();
+        $.post( wcap_salesforce_params.ajax_url, data, function( response ) {
+        	wcap_check_string = response.indexOf("successfuly established");
         	if ( wcap_check_string !== -1 ){
-        		wcap_agile_connection_established = 'yes';
+        		wcap_sf_connection_established = 'yes';
         	}
-    		$( '#wcap_agile_test_connection_message' ).html( response );
-	        $( '#wcap_agile_test_connection_ajax_loader' ).hide();
+    		$( '#wcap_salesforce_test_connection_message' ).html( response );
+	        $( '#wcap_salesforce_test_connection_ajax_loader' ).hide();
         });
 	});
 
 	$ ( '.button-primary' ).on( 'click', function( e ) {
-		if ( $(this).val() == 'Save Agile CRM Settings' ) {			
-			
-			var wcap_agile_user_name = $("#wcap_agile_user_name").val();
-			var wcap_agile_domain    = $("#wcap_agile_domain").val();
-			var wcap_agile_token     = $("#wcap_agile_security_token").val();
-			
-			if ( '' == wcap_agile_token ) {
-				$( "#wcap_agile_security_token_label_error" ).fadeIn();
+		if ( $(this).val() == 'Save Salesforce Setting' ) {			
+			var wcap_sf_user_name = $("#wcap_salesforce_user_name").val();
+			var wcap_sf_password  = $("#wcap_salesforce_password").val();
+			var wcap_sf_token     = $("#wcap_salesforce_security_token").val();
+
+			if ( '' == wcap_sf_token ) {
+				$( "#wcap_salesforce_security_token_label_error" ).fadeIn();
 	            setTimeout( function(){
-		            $( "#wcap_agile_security_token_label_error" ).fadeOut();
+		            $( "#wcap_salesforce_security_token_label_error" ).fadeOut();
 		        },4000);
 		        e.preventDefault();
 			}
-			if ( '' == wcap_agile_domain ) {
-				$( "#wcap_agile_domain_label_error" ).fadeIn();
+			if ( '' == wcap_sf_password ) {
+				$( "#wcap_salesforce_password_label_error" ).fadeIn();
 	            setTimeout( function() {
-		            $( "#wcap_agile_domain_label_error" ).fadeOut();
+		            $( "#wcap_salesforce_password_label_error" ).fadeOut();
 		        },4000);
 			    e.preventDefault();
 			}
-			if ( '' == wcap_agile_user_name ) {
-				$( "#wcap_agile_user_name_label_error" ).fadeIn();
+			if ( '' == wcap_sf_user_name ) {
+				$( "#wcap_salesforce_user_name_label_error" ).fadeIn();
 	            setTimeout( function(){
-		            $( "#wcap_agile_user_name_label_error" ).fadeOut();
+		            $( "#wcap_salesforce_user_name_label_error" ).fadeOut();
 		        },4000);
 	            e.preventDefault();
 			}
 
-			if ( ( wcap_agile_params.wcap_agile_user_name   != wcap_agile_user_name
-				|| wcap_agile_params.wcap_agile_domain_name != wcap_agile_domain 
-				|| wcap_agile_params.wcap_agile_api_key     != wcap_agile_token ) &&
-				 ( wcap_agile_params.wcap_agile_connection_established != 'yes' || wcap_agile_connection_established != 'yes') )  {
+			if ( ( wcap_salesforce_params.wcap_sf_user_name  != wcap_sf_user_name 
+				|| wcap_salesforce_params.wcap_sf_token    != wcap_sf_token
+				|| wcap_salesforce_params.wcap_sf_password != wcap_sf_password ) &&
+				 ( wcap_salesforce_params.wcap_sf_connection_established != 'yes' || wcap_sf_connection_established != 'yes') ){
 				e.preventDefault();
 				var data = {
-		            wcap_agile_user_name: wcap_agile_user_name,
-		            wcap_agile_domain: wcap_agile_domain,
-		            wcap_agile_token: wcap_agile_token,
-		            action: 'wcap_agile_check_connection'
+		            wcap_sf_user_name: wcap_sf_user_name,
+		            wcap_sf_password: wcap_sf_password,
+		            wcap_sf_token: wcap_sf_token,
+		            action: 'wcap_salesforce_check_connection'
 		        };
-		        $( '#wcap_agile_test_connection_ajax_loader' ).show();
-		        $.post( wcap_agile_params.ajax_url, data, function( response ) {
-		    		var wcap_check_string = response.indexOf("successfuly established");
-		    		$( '#wcap_agile_test_connection_ajax_loader' ).hide();
-		    		
-			    	if ( wcap_check_string !== -1 ){
-			    		
-			        	$('#wcap_agile_crm_form').submit();
+		        $( '#wcap_salesforce_test_connection_ajax_loader' ).show();
+		        $.post( wcap_salesforce_params.ajax_url, data, function( response ) {
+
+		    		wcap_check_string = response.indexOf("successfuly established");
+		    		$( '#wcap_salesforce_test_connection_ajax_loader' ).hide();
+		    		if ( wcap_check_string !== -1 ){
+			    		$('#wcap_salesforce_crm_form').submit();
 			        }else{
-			    		$( '#wcap_agile_test_connection_message' ).html( response );
+			    		$( '#wcap_salesforce_test_connection_message' ).html( response );
 					}
 			    });
 			}
 		}
 	});
-
 	var wcap_all = '';
-	$ ( '.add_single_cart' ).on( 'click', function( e ) {
+	$ ( '.add_single_cart_salesforce' ).on( 'click', function( e ) {
 		var wcap_selected_id = [];
-		var wcap_all = '';
 		wcap_selected_id.push ( $( this ).attr( 'data-id' ) );
 		$( '#wcap_manual_email_data_loading' ).show();
 		var data = {
-			action                  : 'wcap_add_to_agile_crm',
+			action                  : 'wcap_add_to_salesforce_crm',
 			wcap_abandoned_cart_ids : wcap_selected_id,
 			wcap_all                : wcap_all
 		};
 
-		$.post( wcap_agile_params.ajax_url, data, function( response ) {
+		$.post( wcap_salesforce_params.ajax_url, data, function( response ) {
 			$( '#wcap_manual_email_data_loading' ).hide();
 			var wcap_check_string = response.indexOf("duplicate_record");
 			if ( wcap_check_string !== -1 ){
 
-        		var display_message       = 'Abandoned cart has been already imported to Agile CRM.';
-				$( ".wcap_agile_message_p_error" ).html( display_message );
-	            $( "#wcap_agile_message_error" ).fadeIn();
+        		var display_message       = 'The Abandoned cart is already exported to Salesforce CRM.';
+				$( ".wcap_salesforce_message_p_error" ).html( display_message );
+	            $( "#wcap_salesforce_message_error" ).fadeIn();
 	            setTimeout( function(){
-	            	$( "#wcap_agile_message_error" ).fadeOut();
+	            	$( "#wcap_salesforce_message_error" ).fadeOut();
 	            },4000);
         	}else{
 				var abadoned_order_count = response;
@@ -111,37 +109,36 @@ jQuery(function( $ ) {
 					order 				 = 'orders';
 				}
 				
-				var display_message       = abadoned_order_count  + ' Abandoned ' +  order + ' has been successfully added to Agile CRM.'
-				$( ".wcap_agile_message_p" ).html( display_message );
-	            $( "#wcap_agile_message" ).fadeIn();
+				var display_message       = abadoned_order_count  + ' Abandoned ' +  order + ' has been successfully added to Salesforce CRM.'
+				$( ".wcap_salesforce_message_p" ).html( display_message );
+	            $( "#wcap_salesforce_message" ).fadeIn();
 	            setTimeout( function(){
-	            	$( "#wcap_agile_message" ).fadeOut();
+	            	$( "#wcap_salesforce_message" ).fadeOut();
 	            },4000);
         	}
 		});
 	});
 
-	$ ( '#add_all_carts' ).on( 'click', function( e ) {
-		
+	$ ( '#add_all_carts_salesforce' ).on( 'click', function( e ) {		
 		wcap_all = 'yes';
 		var wcap_selected_id = [];
 		$( '#wcap_manual_email_data_loading' ).show();
 		var data = {
-			action                  : 'wcap_add_to_agile_crm',
+			action                  : 'wcap_add_to_salesforce_crm',
 			wcap_abandoned_cart_ids : wcap_selected_id,
 			wcap_all                : wcap_all
 		};
 
-		$.post( wcap_agile_params.ajax_url, data, function( response ) {
+		$.post( wcap_salesforce_params.ajax_url, data, function( response ) {
 			$( '#wcap_manual_email_data_loading' ).hide();
 
 			var wcap_check_string = response.indexOf("no_record");
 			if ( wcap_check_string !== -1 ){
-				var display_message       = 'All Abandoned cart has been already imported to Agile CRM.';
-				$( ".wcap_agile_message_p_error" ).html( display_message );
-	            $( "#wcap_agile_message_error" ).fadeIn();
+				var display_message       = 'All the Abandoned carts are already exported to Salesforce CRM. No new carts found to export.';
+				$( ".wcap_salesforce_message_p" ).html( display_message );
+	            $( "#wcap_salesforce_message" ).fadeIn();
 	            setTimeout( function(){
-	            	$( "#wcap_agile_message_error" ).fadeOut();
+	            	$( "#wcap_salesforce_message" ).fadeOut();
 	            },4000);
 			}else{
 				var abadoned_order_count = response;
@@ -150,11 +147,11 @@ jQuery(function( $ ) {
 					order 				 = 'orders';
 				}
 				
-				var display_message       = abadoned_order_count  + ' Abandoned ' +  order + ' has been successfully added to Agile CRM.'
-				$( ".wcap_agile_message_p" ).html( display_message );
-	            $( "#wcap_agile_message" ).fadeIn();
+				var display_message       = abadoned_order_count  + ' Abandoned ' +  order + ' has been successfully added to Salesforce CRM.'
+				$( ".wcap_salesforce_message_p" ).html( display_message );
+	            $( "#wcap_salesforce_message" ).fadeIn();
 	            setTimeout( function(){
-	            	$( "#wcap_agile_message" ).fadeOut();
+	            	$( "#wcap_salesforce_message" ).fadeOut();
 	            },4000);
         	}
 		});
@@ -171,15 +168,15 @@ jQuery(function( $ ) {
 		     
 		     	if ( checkboxes[i].checked ) {
 		     		var email_check = $( checkboxes[i] ).parent().parent().find('.email').text();
-		     		var wcap_agile = email_check.indexOf( "Add to Salesforce CRM" );
-		     		wcap_parent [ checkboxes[i].value ] =  email_check ;
+		     		var wcap_salesforce = email_check.indexOf( "Add to Salesforce CRM" );
+		     		wcap_parent [ checkboxes[i].value ] =  wcap_salesforce ;
 		        	wcap_selected_id.push( checkboxes[i].value );
 		    	}
 		  	}
 
 		  	if ( wcap_selected_id.length == 0 ) {
 		  		var display_message = 'Please select atleast 1 Abandoned order to Add to Salesforce CRM.';
-				$( ".wcap_agile_message_p_error" ).html( display_message );
+				$( ".wcap_salesforce_message_p" ).html( display_message );
 	            $( "#wcap_ac_bulk_message" ).fadeIn();
 	            setTimeout( function(){
 	            	$( "#wcap_ac_bulk_message" ).fadeOut();
@@ -200,9 +197,9 @@ jQuery(function( $ ) {
 				if ( 'no' == allow ) {
 					var display_message = 'Add to Salesforce CRM cannot be applied on Visitor carts.';
 					$( ".wcap_ac_bulk_message_p" ).html( display_message );
-		            $( "#wcap_agile_message_error" ).fadeIn();
+		            $( "#wcap_salesforce_message" ).fadeIn();
 		            setTimeout( function() {
-		            	$( "#wcap_agile_message_error" ).fadeOut();
+		            	$( "#wcap_salesforce_message" ).fadeOut();
 		            },3000);
 					e.preventDefault();
 					return;
@@ -212,22 +209,22 @@ jQuery(function( $ ) {
 		  	var wcap_all = '';
 		  	$( '#wcap_manual_email_data_loading' ).show();
 			var data = {
-				action                  : 'wcap_add_to_agile_crm',
+				action                  : 'wcap_add_to_salesforce_crm',
 				wcap_abandoned_cart_ids : wcap_selected_id,
 				wcap_all                : wcap_all
 			};
 			
-			$.post( wcap_agile_params.ajax_url, data, function( response ) {
+			$.post( wcap_salesforce_params.ajax_url, data, function( response ) {
 				$( '#wcap_manual_email_data_loading' ).hide();
 
 				var wcap_check_string = response.indexOf("duplicate_record");
 				if ( wcap_check_string !== -1 ){
 
-	        		var display_message       = 'Abandoned cart has been already imported to Agile CRM.';
-					$( ".wcap_agile_message_p_error" ).html( display_message );
-		            $( "#wcap_agile_message_error" ).fadeIn();
+	        		var display_message       = 'Selected Abandoned carts are already exported to Salesforce CRM. No new carts found to export.';
+					$( ".wcap_salesforce_message_p_error" ).html( display_message );
+		            $( "#wcap_salesforce_message_error" ).fadeIn();
 		            setTimeout( function(){
-		            	$( "#wcap_agile_message_error" ).fadeOut();
+		            	$( "#wcap_salesforce_message_error" ).fadeOut();
 		            },4000);
 	        	}else{
 					var abadoned_order_count = response;
@@ -236,11 +233,11 @@ jQuery(function( $ ) {
 						order 				 = 'orders';
 					}
 					
-					var display_message       = abadoned_order_count  + ' Abandoned ' +  order + ' has been successfully added to Agile CRM.'
-					$( ".wcap_agile_message_p" ).html( display_message );
-		            $( "#wcap_agile_message" ).fadeIn();
+					var display_message       = abadoned_order_count  + ' Abandoned ' +  order + ' has been successfully added to Salesforce CRM.'
+					$( ".wcap_salesforce_message_p" ).html( display_message );
+		            $( "#wcap_salesforce_message" ).fadeIn();
 		            setTimeout( function(){
-		            	$( "#wcap_agile_message" ).fadeOut();
+		            	$( "#wcap_salesforce_message" ).fadeOut();
 		            },4000);
 	        	}
 			});
@@ -258,16 +255,16 @@ jQuery(function( $ ) {
 		  	for (var i = 0; i < checkboxes.length; i++) {
 		     
 		     	if ( checkboxes[i].checked ) {
-		     		var email_check = $( checkboxes[i] ).parent().parent().find('.email').text();
-		     		var wcap_agile = email_check.indexOf( "Add to Salesforce CRM" );
-		     		wcap_parent [ checkboxes[i].value ] =  email_check ;
+		     		var email_check 	= $( checkboxes[i] ).parent().parent().find('.email').text();
+		     		var wcap_salesforce = email_check.indexOf( "Add to Salesforce CRM" );
+		     		wcap_parent [ checkboxes[i].value ] =  wcap_salesforce ;
 		        	wcap_selected_id.push( checkboxes[i].value );
 		    	}
 		  	}
 
 		  	if ( wcap_selected_id.length == 0 ) {
 		  		var display_message = 'Please select atleast 1 Abandoned order to Add to Salesforce CRM.';
-				$( ".wcap_agile_message_p_error" ).html( display_message );
+				$( ".wcap_salesforce_message_p" ).html( display_message );
 	            $( "#wcap_ac_bulk_message" ).fadeIn();
 	            setTimeout( function(){
 	            	$( "#wcap_ac_bulk_message" ).fadeOut();
@@ -288,9 +285,9 @@ jQuery(function( $ ) {
 				if ( 'no' == allow ) {
 					var display_message = 'Add to Salesforce CRM cannot be applied on Visitor carts.';
 					$( ".wcap_ac_bulk_message_p" ).html( display_message );
-		            $( "#wcap_agile_message_error" ).fadeIn();
+		            $( "#wcap_salesforce_message" ).fadeIn();
 		            setTimeout( function() {
-		            	$( "#wcap_agile_message_error" ).fadeOut();
+		            	$( "#wcap_salesforce_message" ).fadeOut();
 		            },3000);
 					e.preventDefault();
 					return;
@@ -300,21 +297,21 @@ jQuery(function( $ ) {
 		  	var wcap_all = '';
 		  	$( '#wcap_manual_email_data_loading' ).show();
 			var data = {
-				action                  : 'wcap_add_to_agile_crm',
+				action                  : 'wcap_add_to_salesforce_crm',
 				wcap_abandoned_cart_ids : wcap_selected_id,
 				wcap_all                : wcap_all
 			};
 			
-			$.post( wcap_agile_params.ajax_url, data, function( response ) {
+			$.post( wcap_salesforce_params.ajax_url, data, function( response ) {
 				$( '#wcap_manual_email_data_loading' ).hide();
 				var wcap_check_string = response.indexOf("duplicate_record");
 				if ( wcap_check_string !== -1 ){
 
-	        		var display_message       = 'Abandoned cart has been already imported to Agile CRM.';
-					$( ".wcap_agile_message_p_error" ).html( display_message );
-		            $( "#wcap_agile_message_error" ).fadeIn();
+	        		var display_message       = 'Selected Abandoned carts are already exported to Salesforce CRM. No new carts found to export.';
+					$( ".wcap_salesforce_message_p_error" ).html( display_message );
+		            $( "#wcap_salesforce_message_error" ).fadeIn();
 		            setTimeout( function(){
-		            	$( "#wcap_agile_message_error" ).fadeOut();
+		            	$( "#wcap_salesforce_message_error" ).fadeOut();
 		            },4000);
 	        	}else{
 					var abadoned_order_count = response;
@@ -323,11 +320,11 @@ jQuery(function( $ ) {
 						order 				 = 'orders';
 					}
 					
-					var display_message       = abadoned_order_count  + ' Abandoned ' +  order + ' has been successfully added to Agile CRM.'
-					$( ".wcap_agile_message_p" ).html( display_message );
-		            $( "#wcap_agile_message" ).fadeIn();
+					var display_message       = abadoned_order_count  + ' Abandoned ' +  order + ' has been successfully added to Salesforce CRM.'
+					$( ".wcap_salesforce_message_p" ).html( display_message );
+		            $( "#wcap_salesforce_message" ).fadeIn();
 		            setTimeout( function(){
-		            	$( "#wcap_agile_message" ).fadeOut();
+		            	$( "#wcap_salesforce_message" ).fadeOut();
 		            },4000);
 	        	}
 			});
